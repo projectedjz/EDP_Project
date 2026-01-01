188698ec0be0da4a78315020086c860bdd9abf90
Nicholas <nicholaskow21025@gmail.com>
2026-01-01 20:14:41 +0800

    Delete server/LearningAPI directory

--- server/LearningAPI/LearningAPI/Models/AuthResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LearningAPI.Models
-{
-    public class AuthResponse
-    {
-        public UserDTO User { get; set; } = new UserDTO();
-    }
-}
--- server/LearningAPI/LearningAPI/Models/UploadResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LearningAPI.Models
-{
-    public class UploadResponse
-    {
-        public string Filename { get; set; } = string.Empty;
-    }
-}
